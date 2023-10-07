7d4878611eb433db87c57df3b61e7c744f5ec25f
Vo Quoc Nghia <quocnghia91ll@gmail.com>
2023-10-07 20:57:53 +0700

    update command line

--- main.py
+++ main.py
@@ -1,72 +1,73 @@
-import re
+import re  # Import thư viện 're' (regular expressions) để sử dụng cho tìm kiếm và phân tích cú pháp (lexical analysis).
 
-def main():
-    code = read_text_file()
-    print(code)
-    analyze_code(code)
-
-# Function to import the input.txt file
+# Hàm để đọc nội dung từ tệp tin.
 def read_text_file():
-    file_path = "./File/Input.txt"
-    code_builder = []
+    file_path = "./File/Input.txt"  # Đường dẫn tới tệp tin cần đọc.
+    code_builder = []  # Danh sách để lưu trữ nội dung từ tệp tin.
 
     try:
-        # Open the file
+        # Mở tệp tin ở chế độ đọc ("r") và sử dụng 'with' để đảm bảo tệp tin sẽ được đóng đúng cách sau khi hoàn thành.
         with open(file_path, "r") as file_reader:
-            lines = file_reader.readlines()
-            code_builder = "".join(lines)
+            lines = file_reader.readlines()  # Đọc tất cả các dòng từ tệp tin.
+            code_builder = "".join(lines)  # Nối các dòng thành một chuỗi văn bản và lưu vào biến 'code_builder'.
 
     except IOError as e:
-        print(e)
+        print(e)  # In ra thông báo lỗi nếu có lỗi xảy ra trong quá trình đọc tệp tin.
 
-    # Return the content read from the file as a string
-    return code_builder
+    return code_builder  # Trả về nội dung đọc từ tệp tin dưới dạng chuỗi văn bản.
 
+# Hàm để phân tích cú pháp nội dung mã.
 def analyze_code(code):
-    # Setup keywords
-    keyword = "while|if|else|return|break|continue|int|float|void|for"
+    keyword = "while|if|else|return|break|continue|int|float|void|for"  # Danh sách các từ khóa.
 
     letter = "[A-Za-z]"
     digit = "[0-9]"
     underscore = "_"
-    identifier = letter + "(" + letter + "|" + digit + "|" + underscore + ")*"
+    identifier = letter + "(" + letter + "|" + digit + "|" + underscore + ")*"  # Biểu thức chính quy cho từ ngữ (identifier).
 
     digits = digit + "(" + digit + ")*"
     optional_fraction = "(\\." + digits + ")?"
     optional_exponent = "(.)?(E[+|-]?" + digits + ")?"
-    num = "^" + digits + optional_fraction + optional_exponent + "$"
+    num = "^" + digits + optional_fraction + optional_exponent + "$"  # Biểu thức chính quy cho số (num).
 
-    # Create regular expressions for different types of lexemes to be analyzed
+    # Tạo các biểu thức chính quy để nhận dạng từng loại lexeme cần phân tích.
     keyword_pattern = re.compile(r'\b(' + keyword + r')\b')
     identifier_pattern = re.compile(r'\b' + identifier + r'\b')
     num_pattern = re.compile(r'\b' + num + r'\b')
     operator_pattern = re.compile(r'[-+*/=(){}<>;]')
     error_pattern = re.compile(r'[.^a-zA-Z_0-9\-*/=(){}<>;]')
 
-    # Create a list to store lexemes
-    lexemes = []
+    lexemes = []  # Danh sách để lưu trữ các lexeme.
 
-    # Tokenize the code
-    code = re.sub(r'\s', ' ', code)
-    code = re.sub(r'(\b|\s*)([-+*/=(){}<>;])(\b|\s*)', r'\1 \2 \3', code)
-    tokens = code.split()
+    code = re.sub(r'\s', ' ', code)  # Thay thế khoảng trắng và ký tự trắng bằng một dấu cách đơn (' ').
+    code = re.sub(r'(\b|\s*)([-+*/=(){}<>;])(\b|\s*)', r'\1 \2 \3', code)  # Thêm dấu cách xung quanh các toán tử.
+
+    tokens = code.split()  # Tách mã thành các token dựa trên dấu cách.
 
     for token in tokens:
         if keyword_pattern.match(token):
-            lexemes.append(f'keyword : {token}')
-        elif identifier_pattern.match(token):
-            lexemes.append(f'identifier : {token}')
-        elif num_pattern.match(token):
-            lexemes.append(f'num : {token}')
-        elif operator_pattern.match(token):
-            lexemes.append(f'{token} : {token}')
-        elif error_pattern.search(token):
-            lexemes.append(f'Error : {token}')
+            lexemes.append(f'keyword : {token}')  # Nếu token là từ khóa, thêm vào danh sách lexemes với thông tin "keyword :".
 
-    # Print the result
-    print("Class : Lexeme")
+        elif identifier_pattern.match(token):
+            lexemes.append(f'identifier : {token}')  # Nếu token là từ ngữ (identifier), thêm vào danh sách lexemes với thông tin "identifier :".
+
+        elif num_pattern.match(token):
+            lexemes.append(f'num : {token}')  # Nếu token là số (num), thêm vào danh sách lexemes với thông tin "num :".
+
+        elif operator_pattern.match(token):
+            lexemes.append(f'{token} : {token}')  # Nếu token là toán tử, thêm vào danh sách lexemes với thông tin "toán tử : toán tử".
+
+        elif error_pattern.search(token):
+            lexemes.append(f'Error : {token}')  # Nếu token không khớp với các biểu thức chính quy trên, coi là lỗi.
+
+    print("Class : Lexeme")  # In tiêu đề cột.
     for lexeme in lexemes:
-        print(lexeme)
+        print(lexeme)  # In danh sách các lexeme đã tìm thấy.
+
+def main():
+    code = read_text_file()  # Đọc nội dung từ tệp tin và lưu vào biến 'code'.
+    print(code)  # In nội dung đã đọc từ tệp tin.
+    analyze_code(code)  # Gọi hàm 'analyze_code' để phân tích cú pháp nội dung mã.
 
 if __name__ == "__main__":
-    main()
+    main()  # Gọi hàm chính khi chương trình được chạy.
